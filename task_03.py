e4a8a019712dd9c18b5a9c943e5b488436b0abe7
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-11 10:41:22 +0000

    I hope this goes through

---
+++ task_03.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 03: For loop"""
+
+from collections import Counter
+import decimal
+
+def lexicographics(to_analyze):
+    """a **required** string here in the great wild"""
+    lines = 0
+    for length in to_analyze:
+        #length = str()
+        #length.split('\n')
+        #length.strip(to_analyze)
+        #max_length = max(length)
+        #min_length = min(length)
+    #return counter.max_length, counter.min_length
+        length = length.split('\n')
+        #lines += 1
+        #count = len(to_
+        #to_analyze = to_analyze.split()
+        return max(len(length)), min(len(length))
+    #decimal.Decimal(sum(length))
