1659eb6a8877745cb22b070ecbd61b06ea870324
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-11 04:47:40 +0000

    I hope this goes through

---
+++ task_02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 02: If and Else"""
+
+
+def bool_to_str(bval):
+    """Boolean function"""
+    if bval is True:
+        return "Yes"
+    else:
+        return "No"
